eaffdada0d3e611243e7da50c37eefeab658dbf0
veerraju <67301830+veerraju418@users.noreply.github.com>
2022-03-11 11:15:42 +0530

    Update Testarray.java

--- Testarray.java
+++ Testarray.java
@@ -2,11 +2,11 @@ import java.io.*;
 class Testarray{  
 public static void main(String args[]){  
 int a[]=new int[5];//declaration and instantiation  
-a[0]=10;//initialization  
-a[1]=20;  
-a[2]=70;  
-a[3]=40;  
-a[4]=50;  
+a[0]=1;//initialization  
+a[1]=2;  
+a[2]=7;  
+a[3]=4;  
+a[4]=5;  
 //traversing array  
 for(int i=0;i<a.length;i++)//length is the property of array  
 System.out.println(a[i]);  
